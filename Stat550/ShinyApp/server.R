4bd2c423de21a690fe030fae9c8ff71abfcca940
Wenzheng Zhou <wenzheng.zhou@stat.ubc.ca>
2018-03-22 15:45:44 -0700

    update

--- Stat550/ShinyApp/server.R
+++ Stat550/ShinyApp/server.R
@@ -4,6 +4,12 @@ library(stringr)
 library(shinyjs)
 library(shiny)
 
-server <- function(input, output) {
-  
+server <- function(input, output){
+  output$num_results <- renderText({ 
+    n<-nrow()
+    paste("We filtered out",n,"provinces")
+  })
+  output$MapPlot <- renderPlot()
+  output$table_head <- DT::renderDataTable()
+  output$DataDownload <- downloadHandler()
 }
