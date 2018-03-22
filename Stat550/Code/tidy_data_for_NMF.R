7255a822d9d78065d5986bc42f4ef3e508c44c98
Shanshan Pi <shanshan.pi@stat.ubc.ca>
2018-03-22 00:39:24 -0700

    revise

--- Stat550/Code/tidy_data_for_NMF.R
+++ Stat550/Code/tidy_data_for_NMF.R
@@ -20,7 +20,7 @@ rownames(tax_LTB_all_tidy) <- allYears
 
 write.csv(tax_LTB_all_tidy, file="./Data/nmf_data/tax_LTB_all_tidy.csv")
 
-#Tidy data
+#Tidy data for STB
 colnames(tax_data_STB)[3] <- 'Number.of.Staff'
 tax_STB_all <- tax_data_STB[, c(1:3)]
 tax_STB_all <- rbind(tax_STB_all[1:26, ], c("1996", "Chongqing", NA), tax_STB_all[27:nrow(tax_STB_all), ])
